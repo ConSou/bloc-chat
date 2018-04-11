98cb6513f221b013a6adbcc7a6e3d8820f23e30a
Conor Souhrada <ConorSouhrada@gmail.com>
2018-04-11 16:23:36 -0600

    checkpoint-2-listChat

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 import * as firebase from 'firebase';
 import RoomList from './components/RoomList';
 import './App.css';
-src="https://www.gstatic.com/firebasejs/4.12.1/firebase.js"
+
+var config = {
+   apiKey: "AIzaSyAkWKKpFeIHEUHXnh7J5AUk2y6Kd6htyDs",
+   authDomain: "bloc-chat-f24c0.firebaseapp.com",
+   databaseURL: "https://bloc-chat-f24c0.firebaseio.com",
+   projectId: "bloc-chat-f24c0",
+   storageBucket: "bloc-chat-f24c0.appspot.com",
+   messagingSenderId: "730929209826"
+ };
+ firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Bloc Chat
-        <RoomList />
+      <div className='holder'>
+        <h1 className="bloc-chat">Bloc Chat</h1>
+        <RoomList firebase={firebase} />
+      </div>
       </div>
     );
   }
--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -7,11 +7,25 @@ class RoomList extends Component {
     this.state = {
       rooms: []
     }
+    this.roomsRef = this.props.firebase.database().ref('rooms');
   }
+
+  componentDidMount() {
+    this.roomsRef.on('child_added', snapshot => {
+      const room = snapshot.val();
+      room.key = snapshot.key;
+      this.setState({ rooms: this.state.rooms.concat( room ) });
+  });
+}
+
   render() {
     return (
       <div className="room-list">
-        RoomList
+      {
+        this.state.rooms.map((name, index) =>
+          <h1 className="room-names" key={index}>{this.state.rooms[index].name}</h1>
+        )
+      }
       </div>
     );
   }
