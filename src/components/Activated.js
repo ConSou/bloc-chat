cefc8d9bc08b1b1776bd61c7dd1decedd100f096
Conor Souhrada <ConorSouhrada@gmail.com>
2018-04-17 14:21:05 -0600

    checkpoint-4-commit

--- src/components/Activated.js
+++ src/components/Activated.js
@@ -3,44 +3,27 @@ import React, { Component } from 'react';
 class Activated extends Component {
 
   render() {
- let activeMessage = [];
-    for(var x in this.props.messages){
-      let activeMessageId = this.props.messages[x].roomId;
-        if(this.props.activeRoom.key === activeMessageId){
-          activeMessage.push(this.props.messages[x])
-          console.log(activeMessage)
-          continue;}
 
-      for(var i = 0; i < activeMessage.length; i++){
-        console.log(activeMessage)
-        if(this.props.activeRoom.key === activeMessage[i].roomId){
-        return (
-          <div className="activated">
-          <h2> {this.props.activeRoom.name} </h2>
-          <table>
+    let filtMes = this.props.messages.filter(val => val.roomId === this.props.activeRoom.key)
+
+  return (
+    <div className="activated">
+      <h2> {this.props.activeRoom.name} </h2>
+      <div>{filtMes.map((val, index) =>
+          <table key={index}>
             <tbody>
-            {
-              activeMessage.map((content, index) =>
-              <tr key={index}>
-                <td> {activeMessage[index].userName} </td>
-                <td> {activeMessage[index].content} </td>
-                <td> {activeMessage[index].sentAt} </td>
+              <tr>
+                <td> {val.userName} </td>
+                <td> {val.content} </td>
+                <td> {val.sentAt} </td>
               </tr>
-            )
-            }
             </tbody>
           </table>
-          </div>
-        )
-      }
-    }
-    }
-    return (
-      <div className="activated">
-      <h2> {this.props.activeRoom.name} </h2>
-      <p> You have no messages </p>
+        )}
       </div>
-      );
+    </div>
+);
+
     }
   }
 
